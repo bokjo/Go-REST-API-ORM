51d8077a6621d50df23b1e80b9e5bb38b293d8b6
Bojanche Stojchevski <bstojcevski89@gmail.com>
2018-03-07 17:07:27 +0100

    established connection to postgres as test

--- dbHandler.go
+++ dbHandler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
@@ -11,6 +12,7 @@ var db *gorm.DB
 var err error
 
 func dbConnect() *gorm.DB {
+	// db, err := gorm.Open("postgres", "user=bstojchevski password=Password1 dbname=postgres sslmode=disable")
 	db, err := gorm.Open("sqlite3", "user.db")
 	if err != nil {
 		panic(fmt.Sprintf("Cannot connect to the database!\n ERROR: '%v'", err))
